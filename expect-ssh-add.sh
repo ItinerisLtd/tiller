3a51155979c7e8acfde69da7177f1650e56cd84d
Tang Rufus <tangrufus@gmail.com>
2018-05-14 22:17:24 +0800

    `expect-ssh-add.sh`: Fix bad passphase input

--- expect-ssh-add.sh
+++ expect-ssh-add.sh
@@ -4,7 +4,7 @@ set file_name [lindex $argv 0]
 set passphrase [lindex $argv 1]
 
 spawn ssh-add /root/.ssh/$file_name
-expect "Enter passphrase"
-send $passphrase"\n"
+expect "Enter passphrase for /root/.ssh/$file_name:"
+send $passphrase\n
 expect "Identity added"
 interact
